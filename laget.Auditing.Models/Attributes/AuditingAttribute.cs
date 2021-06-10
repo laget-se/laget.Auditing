bb27c8027c33b0590c214bbe727afaea6d0a9a17
Erik Hennerfors <erik.hennerfors@obscured.se>
2021-06-10 13:10:14 +0200

    Added missing files :O

---
+++ laget.Auditing.Models/Attributes/AuditingAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace laget.Auditing.Models.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+
+    public class AuditingAttribute : Attribute
+    {
+        public string Name { get; set; }
+
+        public AuditingAttribute(string name)
+        {
+            Name = name;
+        }
+    }
+}
