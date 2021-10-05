ae200afcac535c035068e29053cb38862d109a35
Erik Hennerfors <erik.hennerfors@obscured.se>
2021-10-05 12:05:13 +0200

    Moved each function (Elasticsearch and MongoDB) to seperate files

---
+++ laget.Auditing.Persistor/Functions/Elasticsearch.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace laget.Auditing.Persistor.Functions
+{
+    public static class Elasticsearch
+    {
+        [FunctionName("Elasticsearch")]
+        public static void Run([ServiceBusTrigger("auditing", "sink-elasticsearch", Connection = "AzureServiceBusConnectionString")] string mySbMsg, ILogger log)
+        {
+            log.LogInformation($"C# ServiceBus topic trigger function processed message: {mySbMsg}");
+        }
+    }
+}
---
+++ laget.Auditing.Persistor/Functions/MongoDB.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace laget.Auditing.Persistor.Functions
+{
+    public static class MongoDB
+    {
+        [FunctionName("MongoDB")]
+        public static void Run([ServiceBusTrigger("auditing", "sink-mongodb", Connection = "AzureServiceBusConnectionString")]string mySbMsg, ILogger log)
+        {
+            log.LogInformation($"C# ServiceBus topic trigger function processed message: {mySbMsg}");
+        }
+    }
+}
