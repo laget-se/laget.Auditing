d72b14e880c5dba0070d4bf2f312c0902534f26c
Morgan Lundin <morgan.lundin@laget.se>
2021-10-06 10:47:02 +0200

    Added Startup, added nugets Microsoft.Azure.Functions.Extensions and Microsoft.Extensions.DependencyInjection

---
+++ laget.Auditing.Persistor/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(laget.Auditing.Persistor.Functions.Startup))]
+
+namespace laget.Auditing.Persistor.Functions
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+        }
+    }   
+}
