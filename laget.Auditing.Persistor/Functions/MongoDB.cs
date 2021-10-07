8a751f1252fdadebce3d92d6ad36cce000a6a25a
Erik Hennerfors <erik.hennerfors@obscured.se>
2021-10-07 08:23:42 +0200

    Reverted TargetFramework from net5.0 to netstandard2.1 and netcoreapp3.1 since Azure Function fails under net5.0

--- laget.Auditing.Persistor/Functions/MongoDB.cs
+++ laget.Auditing.Persistor/Functions/MongoDB.cs
@@ -1,29 +1,29 @@
-﻿//using System;
-//using laget.Auditing.Sinks;
-//using laget.Auditing.Sinks.MongoDB.Models;
-//using Microsoft.Azure.WebJobs;
-//using Microsoft.Extensions.Logging;
-//using StatsdClient;
+﻿using System;
+using laget.Auditing.Sinks;
+using laget.Auditing.Sinks.MongoDB.Models;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using StatsdClient;
 
-//namespace laget.Auditing.Persistor.Functions
-//{
-//    public class MongoDB
-//    {
-//        private readonly IPersistor<Message> _persistor;
+namespace laget.Auditing.Persistor.Functions
+{
+    public class MongoDB
+    {
+        private readonly IPersistor<Message> _persistor;
 
-//        public MongoDB()
-//        {
-//            _persistor = new Sinks.MongoDB.Persistor(Environment.GetEnvironmentVariable("MongoConnectionString"));
-//        }
+        public MongoDB()
+        {
+            _persistor = new Sinks.MongoDB.Persistor(Environment.GetEnvironmentVariable("MongoConnectionString"));
+        }
 
-//        [FunctionName("MongoDB")]
-//        public void Run([ServiceBusTrigger("auditing", "sink-mongodb", Connection = "AzureServiceBus")]Message message, ILogger log)
-//        {
-//            log.LogInformation($"C# ServiceBus topic trigger function processed message: {message}");
-//            DogStatsd.Counter("sink.mongodb.message", 1);
+        [FunctionName("MongoDB")]
+        public void Run([ServiceBusTrigger("auditing", "sink-mongodb", Connection = "AzureServiceBus")] Message message, ILogger log)
+        {
+            log.LogInformation($"C# ServiceBus topic trigger function processed message: {message}");
+            DogStatsd.Counter("sink.mongodb.message", 1);
 
 
-//            _persistor.Persist(message.Name, message);
-//        }
-//    }
-//}
+            _persistor.Persist(message.Name, message);
+        }
+    }
+}
