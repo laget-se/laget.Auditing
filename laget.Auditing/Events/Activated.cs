ccc53e0cfc7cc6c4d6d6f103c3e37399e2d7c987
Erik Hennerfors <erik.hennerfors@laget.se>
2021-11-03 13:12:45 +0100

    Added event Activated and Deactivated

---
+++ laget.Auditing/Events/Activated.cs
@@ -0,0 +1,22 @@
+﻿using laget.Auditing.Models;
+using Newtonsoft.Json;
+
+namespace laget.Auditing.Events
+{
+    public class Activated : Event
+    {
+        [JsonProperty("action")]
+        public override string Action { get; set; } = nameof(Activated);
+
+        public Activated(string name, object entity)
+            : base(name, entity)
+        {
+        }
+
+        public Activated(string name, object entity, string description)
+            : base(name, entity)
+        {
+            Description = description;
+        }
+    }
+}
---
+++ laget.Auditing/Events/Deactivated.cs
@@ -0,0 +1,22 @@
+﻿using laget.Auditing.Models;
+using Newtonsoft.Json;
+
+namespace laget.Auditing.Events
+{
+    public class Deactivated : Event
+    {
+        [JsonProperty("action")]
+        public override string Action { get; set; } = nameof(Deactivated);
+
+        public Deactivated(string name, object entity)
+            : base(name, entity)
+        {
+        }
+
+        public Deactivated(string name, object entity, string description)
+            : base(name, entity)
+        {
+            Description = description;
+        }
+    }
+}
