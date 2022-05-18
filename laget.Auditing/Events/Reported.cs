59177c38818035a00484ef22b4b1190595af8297
morganlun <86594249+morganlun@users.noreply.github.com>
2022-05-18 10:17:29 +0200

    Reported added to events (#59)

---
+++ laget.Auditing/Events/Reported.cs
@@ -0,0 +1,22 @@
+﻿using laget.Auditing.Models;
+using Newtonsoft.Json;
+
+namespace laget.Auditing.Events
+{
+    public class Reported : Event
+    {
+        [JsonProperty("action")]
+        public override string Action { get; set; } = nameof(Reported);
+
+        public Reported(string name, object entity)
+            : base(name, entity)
+        {
+        }
+
+        public Reported(string name, object entity, string description)
+            : base(name, entity)
+        {
+            Description = description;
+        }
+    }
+}
