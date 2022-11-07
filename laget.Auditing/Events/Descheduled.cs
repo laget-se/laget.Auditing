0b18727d0a92318e50aa4b851e412fc5ed84e4f9
Erik Hennerfors <erik.hennerfors@laget.se>
2022-11-07 06:29:43 +0100

    Updated nuget(s)

---
+++ laget.Auditing/Events/Descheduled.cs
@@ -0,0 +1,22 @@
+﻿using laget.Auditing.Models;
+using Newtonsoft.Json;
+
+namespace laget.Auditing.Events
+{
+    public class Descheduled : Event
+    {
+        [JsonProperty("action")]
+        public override string Action { get; set; } = nameof(Descheduled);
+
+        public Descheduled(string name, object entity)
+            : base(name, entity)
+        {
+        }
+
+        public Descheduled(string name, object entity, string description)
+            : base(name, entity)
+        {
+            Description = description;
+        }
+    }
+}
---
+++ laget.Auditing/Events/Scheduled.cs
@@ -0,0 +1,22 @@
+﻿using laget.Auditing.Models;
+using Newtonsoft.Json;
+
+namespace laget.Auditing.Events
+{
+    public class Scheduled : Event
+    {
+        [JsonProperty("action")]
+        public override string Action { get; set; } = nameof(Scheduled);
+
+        public Scheduled(string name, object entity)
+            : base(name, entity)
+        {
+        }
+
+        public Scheduled(string name, object entity, string description)
+            : base(name, entity)
+        {
+            Description = description;
+        }
+    }
+}
